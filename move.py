ad1d6f6bc3acaa7089a9b2ad1e40d57f9ab97cfc
Pob4acke <97384421+Pob4acke@users.noreply.github.com>
2022-01-09 22:14:13 +0100

    Added Feature to check actual height at the end
    height will be checked as soon as the desk stops.
    This height will then be forwarded to home bridge

--- move.py
+++ move.py
@@ -1,10 +1,21 @@
 import sys
 import subprocess
 
+### calc percentage to height
 height_rel = sys.argv[1]
-height = height_rel
-height = 620 + float(height)/100*650
+height = 620 + float(height_rel)/100*650
 height = int(height)
-print(height_rel) #output for homekit plugin
+if height == 620:
+        height = 621 #in some cases idasen-control gets an exception when moving to lowest position (due to some overdirve?)
 
-subprocess.call("/home/pi/.local/bin/idasen-controller --move-to " + str(height) + " > /dev/null &", shell=True)
+### call idasen-controler with height
+cmd = subprocess.Popen("/home/pi/.local/bin/idasen-controller --move-to " + str(height), shell=True, stdout=subprocess.PIPE)
+
+### calc new height back to percantage value as return
+for line in cmd.stdout:
+        if "Final height: " in line: # excample line in output from idasen-controler "Final height:  750mm (Target:  750mm)"
+                height_str = line[14:].split("m")[0] #remove first 14 chars and use the string till first "m" --> the acutal height in mm
+                height = int(height_str)
+                height_rel = float(height)/6.5 - 95
+                height_rel = int(height_rel)
+                print(height_rel) 
